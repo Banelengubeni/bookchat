984ed82f4d9ee7643c4d3d0069d769722227ad25
Banelengubeni <155159669+Banelengubeni@users.noreply.github.com>
2024-04-07 17:51:08 +0200

    Update sign-in.js

--- sign-in.js
+++ sign-in.js
@@ -12,8 +12,4 @@ document
     console.log("Username:", username);
     console.log("Password:", password);
     window.location.href = "dashboard.html";
-
-    // After successful login, you can redirect the user to another page
-    // For demonstration purposes, let's just reload the current page
-    // window.location.href = "dashboard.html";
   });
